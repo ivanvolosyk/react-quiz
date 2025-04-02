859f8eb835da88644d525a48e3454527f48307be
Ivan Volosyk <volosyk.vanya@gmail.com>
2025-04-02 17:06:48 +0300

    Add Question component

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,13 @@ import Main from "./Main";
 import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
+import Question from "./Question";
 
 const intialState = {
   questions: {},
 
   status: "loading",
+  index: 0,
 };
 
 function reducer(state, action) {
@@ -17,13 +19,18 @@ function reducer(state, action) {
       return { ...state, questions: action.payload, status: "ready" };
     case "dataFailed":
       return { ...state, status: "error" };
+    case "start":
+      return { ...state, status: "active" };
     default:
       throw new Error("Action is unknown");
   }
 }
 
 export default function App() {
-  const [{ questions, status }, dispatch] = useReducer(reducer, intialState);
+  const [{ questions, status, index }, dispatch] = useReducer(
+    reducer,
+    intialState
+  );
 
   const numQuestions = questions.length;
 
@@ -39,7 +46,10 @@ export default function App() {
       <Main>
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
-        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && <Question question={questions[index]} />}
       </Main>
     </div>
   );
